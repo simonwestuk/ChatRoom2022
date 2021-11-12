418fb0e65c4f27e2a21eda21ab6a7c7415c97f42
wests <wests@C-010053>
2021-11-12 15:51:12 +0000

    Add project files.

---
+++ ChatRoom3/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom3.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+    }
+}
